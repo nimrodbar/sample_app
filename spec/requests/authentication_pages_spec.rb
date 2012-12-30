9291a93fd694f78398c720d3c33e68d002256a0e
Nimrod Bar <nimrod.bar@gmail.com>
2012-12-30 13:51:48 +0200

    Chapter 9 exerices

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,6 +51,12 @@ describe "Authentication" do
 
       describe "in the Users controller" do
 
+        describe "no account links" do
+          it { should_not have_link('Settings', href: edit_user_path(user)) }
+          it { should_not have_link('Sign out', href: signout_path) }
+          it { should_not have_link('Profile',  href: user_path(user)) }
+        end
+
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
