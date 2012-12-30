8c22aacb2cac465812990c5f489886f77ccff1f5
Nimrod Bar <nimrod.bar@gmail.com>
2012-12-30 13:27:27 +0200

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,10 +25,12 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "remember token" do
     before { @user.save }
@@ -125,5 +127,14 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 end
